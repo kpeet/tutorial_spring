f14374cf0874827ae950ddfcd13b2b9ba321e6bd
Kenneth Peet D. <kpeet@inf.utfsm.cl>
2015-12-24 16:43:32 -0300

    Tutorial 21 Stereotypes parte 2 Complemento
    Recomendación de Buena práctica al trabajar con capas.
    
    @Component: para definir un bean de manera genérica
    @Repository: Para la capa de datos, donde es común usar el patrón DAO.
    @ Controller: en la capa controlador, con alusión al patrón MVC.
    @ Service:para la capa que va a consumir nuestros accesos a datos.
    Pero todas hacen el mismo efecto, solo sirven para hacer una
    diferenciación de capas.
    
    Modificaciones:
    
    Barcelona.java: Se deja commentado para probar con los distintos
    stereotypes, y demostrar que todos hacen lo mismo.

--- spring/src/main/java/com/ejemplos/beans/Barcelona.java
+++ spring/src/main/java/com/ejemplos/beans/Barcelona.java
@@ -1,10 +1,20 @@
 package com.ejemplos.beans;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 
 import com.ejemplos.springframe.interfaces.IEquipo;
 
-@Component
+@Controller //Controller - Service - Repository
+/*
+ @Component: para definir un bean de manera genérica
+@Repository: Para la capa de datos, donde es común usar el patrón DAO.
+@ Controller: en la capa controlador, con alusión al patrón MVC.
+@ Service:para la capa que va a consumir nuestros accesos a datos.
+Pero todas hacen el mismo efecto, solo sirven para hacer una diferenciación de capas.
+
+ */
 public class Barcelona implements IEquipo{
 
 	@Override
