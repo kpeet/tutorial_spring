247a9fcf723af7bd74c4c29980c0648345b6bdff
Kenneth Peet D. <kpeet@inf.utfsm.cl>
2015-12-24 17:51:00 -0300

    Tutorial 22 Ejercicio 1 Xml seguda parte. Cambio Dínamico de Equipo.
    Esto es solucionado solo con un poco de lógica desde la App principal.
    
    Entonces, tenemos una configuración inicial registrada en el beans.xml,
    pero puede ser modificada gracias a una inyección de dependencias que
    estamos haciendo bajo el enfoque de interfaces, lo que nos da la
    flexibiliad a nuestro código y poder cambiar de instancia de acuerdo a
    la implementación.
    
    Modificaciones:
    
    App.java: modificaciones de logica para generar inyecciones de
    dependencia por medio de interfaces.
    
    Camiseta.java y Marca.java, al parecer no fueron cargados en el commit
    anterior, ojo si no resulta su absoluta compilacion en el commit
    anterior.

--- spring/src/main/java/com/ejemplos/springframe/spring/App.java
+++ spring/src/main/java/com/ejemplos/springframe/spring/App.java
@@ -1,17 +1,37 @@
 package com.ejemplos.springframe.spring;
 
+import java.util.Scanner;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.ejemplos.beans.Barcelona;
 import com.ejemplos.beans.Jugador;
+import com.ejemplos.beans.Juventus;
 
 public class App {
 
 	public static void main(String[] args) {
 		
 		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/ejemplos/springframe/xml/beans.xml");
-		Jugador jug = (Jugador) appContext.getBean("jugador1");//llamo al alias o apodo
+		Jugador jug = (Jugador) appContext.getBean("jugador1");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Elija un equipo: 1- Barcelona 2- Juventus");
+		int respuesta = sc.nextInt();
+		switch(respuesta){
+		case 1:
+			jug.setEquipo(new Barcelona());
+			break;
+		case 2:
+			jug.setEquipo(new Juventus());
+			break;
+		default:
+			break;
+		}
+		
+		
 		
 		//IEquipo bar =(IEquipo) appContext.getBean("barcelona");//juventus
 		System.out.println(jug.getNombre() +"-"+jug.getEquipo().mostrar()+" - "+jug.getCamiseta().getNumero()+" - "+jug.getCamiseta().getMarca().getNombre() );
