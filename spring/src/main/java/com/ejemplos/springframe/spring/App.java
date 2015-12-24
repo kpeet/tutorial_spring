bd3b47f5864786895b22a9235e99e0451ec54956
Kenneth Peet D. <kpeet@inf.utfsm.cl>
2015-12-23 21:46:45 -0300

    Tutorial 18 - Uso de interfaces segunda parte, ejemplo práctico
    las interfaces permiten tener diferentes instancias de objetos sin estar
    codificando muchas referencias
    
    IEquipo bar =(IEquipo) appContext.getBean("barcelona");//Juventus
    
    Modificaciones:
    
    App.java:Modificación de la información presentada de un equipo, para
    funcionar con la interface IEquipo
    
    beans.xml:Carga del bean juventus

--- spring/src/main/java/com/ejemplos/springframe/spring/App.java
+++ spring/src/main/java/com/ejemplos/springframe/spring/App.java
@@ -1,19 +1,23 @@
 package com.ejemplos.springframe.spring;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.ejemplos.beans.Jugador;
-import com.ejemplos.beans.Persona;
+
+import com.ejemplos.springframe.interfaces.IEquipo;
 
 public class App {
 
 	public static void main(String[] args) {
 		
 		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/ejemplos/springframe/xml/beans.xml");
-		Jugador jug = (Jugador) appContext.getBean("messi");//llamo al alias o apodo
-		
-		System.out.println(jug.getNombre() +"-"+jug.getEquipo().mostrar());
+		//Jugador jug = (Jugador) appContext.getBean("messi");//llamo al alias o apodo
+		IEquipo bar =(IEquipo) appContext.getBean("barcelona");//juventus
+		//System.out.println(jug.getNombre() +"-"+jug.getEquipo().mostrar());
+		System.out.println(bar.mostrar());
+		//Ejemplo de como porder usar las interfaces en spring
+		((ConfigurableApplicationContext) appContext).close();
 	
 
 	}
